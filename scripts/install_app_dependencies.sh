7ce072742cf38ce6a45ad0d00f8a5c9f2b1b8654
alexmax13 <oleksandrmax13@gmail.com>
2022-06-14 14:43:25 +0300

    add sudo to venv install, change dir install requirements.txt

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo pip3 install virtualenv
 cd /home/ubuntu/django-blog/s_blog/
-virtualenv environment
-source environment/bin/activate
+sudo virtualenv environment
+sudo source environment/bin/activate
+cd /home/ubuntu/django-blog
 sudo pip3 install -r requirements.txt
